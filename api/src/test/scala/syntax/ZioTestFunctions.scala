83b0f97436b55046b1f68516894e7adfc868b2c4
Filippo Mariotti <filippo.mariotti@gmail.com>
2020-03-05 10:46:53 +0000

    Zio 1.0.0-RC18-1

--- api/src/test/scala/syntax/ZioTestFunctions.scala
+++ api/src/test/scala/syntax/ZioTestFunctions.scala
@@ -4,13 +4,15 @@ import org.scalatest.Assertion
 import org.scalatest.matchers.should.Matchers
 import zio.IO
 
-sealed abstract private[syntax] class ZioTestFunctions extends Matchers with zio.DefaultRuntime {
+sealed abstract private[syntax] class ZioTestFunctions extends Matchers {
   final def successAssert[E, A](io: IO[E, A])(assert: A => Assertion): Assertion =
-    unsafeRun(io.either)
+    zio.Runtime.default
+      .unsafeRun(io.either)
       .fold(err => fail(s"Expected success but got $err"), assert)
 
   final def failureAssert[E, A](io: IO[E, A])(assert: E => Assertion): Assertion =
-    unsafeRun(io.either)
+    zio.Runtime.default
+      .unsafeRun(io.either)
       .fold(assert, res => fail(s"Expected failure but got $res"))
 }
 
